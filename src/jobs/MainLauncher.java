06fa3f8219a0ba897f9624e8718ac632a8ebc054
who(true) <alexkjh2011@gmail.com>
2022-08-25 01:33:53 +0900

    야가다 전직(프로그래머)

--- src/jobs/MainLauncher.java
+++ src/jobs/MainLauncher.java
@@ -4,7 +4,7 @@ public class MainLauncher {
     public static void main(String[] args) {
         Person person = new Person("민우", 30, 176);
         person.introduceName();
-        person = new Nogada("정훈", 24, 185);
+        person = new Programmer("정훈", 24, 185);
         person.introduceName();
 
 
