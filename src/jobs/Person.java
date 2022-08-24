d5ea426a16838fb6147cd9974ef6922a0967a304
who(true) <alexkjh2011@gmail.com>
2022-08-25 00:57:21 +0900

    person 구상 +인사

---
+++ src/jobs/Person.java
@@ -0,0 +1,19 @@
+package jobs;
+
+public class Person {
+    String name;
+    int age;
+    int height;
+
+    public Person(String name, int age, int height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+
+    }
+
+    public void introduceName(){
+        System.out.println("my name is " + this.name);
+
+    }
+}
