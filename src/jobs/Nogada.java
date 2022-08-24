99e139154fdc9fb3ad0c637d128516d551aa79bd
who(true) <alexkjh2011@gmail.com>
2022-08-25 01:28:45 +0900

    야가다 추가

---
+++ src/jobs/Nogada.java
@@ -0,0 +1,13 @@
+package jobs;
+
+public class Nogada extends Person{
+    public Nogada(String name, int age, int height) {
+        super(name, age, height);
+
+    }
+
+    public void introduceName(){
+        System.out.println("ma name " + this.name);
+
+    }
+}
