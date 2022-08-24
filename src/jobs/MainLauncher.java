2eb36428af4613f8c93b1a4a1f13a402b53ca670
who(true) <alexkjh2011@gmail.com>
2022-08-25 01:29:11 +0900

    메인에 야가다 추가

--- src/jobs/MainLauncher.java
+++ src/jobs/MainLauncher.java
@@ -4,5 +4,9 @@ public class MainLauncher {
     public static void main(String[] args) {
         Person person = new Person("민우", 30, 176);
         person.introduceName();
+        person = new Nogada("정훈", 24, 185);
+        person.introduceName();
+
+
     }
 }
