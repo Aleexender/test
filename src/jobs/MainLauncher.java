48911ac1993ec371e0f6f129e7c11a98674f8db0
who(true) <alexkjh2011@gmail.com>
2022-08-25 01:00:27 +0900

    test 민우쌤

--- src/jobs/MainLauncher.java
+++ src/jobs/MainLauncher.java
@@ -2,6 +2,7 @@ package jobs;
 
 public class MainLauncher {
     public static void main(String[] args) {
-        
+        Person person = new Person("민우", 30, 176);
+        person.introduceName();
     }
 }
