ab6e981a66bc839cac7e85ca4df25f6abad41a41
who(true) <alexkjh2011@gmail.com>
2022-08-25 01:01:51 +0900

    정정

--- src/jobs/MainLauncher.java
+++ src/jobs/MainLauncher.java
@@ -2,7 +2,7 @@ package jobs;
 
 public class MainLauncher {
     public static void main(String[] args) {
-        Person person = new Person("민우", 30, 176);
+        Person person = new Person("민우", 30, 180);
         person.introduceName();
     }
 }
