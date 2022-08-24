d31b93b6c21767b0344a2f77597936e7b8b7b40e
who(true) <alexkjh2011@gmail.com>
2022-08-25 01:03:21 +0900

    Revert "정정"
    This reverts commit ab6e981a66bc839cac7e85ca4df25f6abad41a41.

--- src/jobs/MainLauncher.java
+++ src/jobs/MainLauncher.java
@@ -2,7 +2,7 @@ package jobs;
 
 public class MainLauncher {
     public static void main(String[] args) {
-        Person person = new Person("민우", 30, 180);
+        Person person = new Person("민우", 30, 176);
         person.introduceName();
     }
 }
