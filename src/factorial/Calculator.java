3931a8f7dafdd3581ebb18e9fa5da5f37ba6196e
who(true) <alexkjh2011@gmail.com>
2022-08-26 23:19:42 +0900

    put Scanner + sout

--- src/factorial/Calculator.java
+++ src/factorial/Calculator.java
@@ -1,7 +1,11 @@
 package factorial;
 
+import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args) {
+        System.out.println("팩토리얼 계산을 위한 정수값 입력");
+        Scanner sc = new Scanner(System.in);
         
     }
 }
