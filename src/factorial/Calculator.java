27006c15e803b7a7a0ec79c96ccebe0007e27cee
who(true) <alexkjh2011@gmail.com>
2022-08-26 23:20:13 +0900

    data setting

--- src/factorial/Calculator.java
+++ src/factorial/Calculator.java
@@ -6,6 +6,7 @@ public class Calculator {
     public static void main(String[] args) {
         System.out.println("팩토리얼 계산을 위한 정수값 입력");
         Scanner sc = new Scanner(System.in);
-        
+        int num = sc.nextInt();
+        int fac = 1; //초기값 설정
     }
 }
