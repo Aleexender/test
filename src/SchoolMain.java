81c2a7ef3f127e8889f1a7f1ccac27bfb74d8872
who(true) <alexkjh2011@gmail.com>
2022-08-29 00:37:24 +0900

    delete main

--- src/SchoolMain.java
+++ src/SchoolMain.java
@@ -1,8 +1,6 @@
 public class SchoolMain {
     public static void main(String[] args) {
-        Teacher teacher = new Student("철수");
-        Student student = (Student) new Teacher(); // 캐스팅
-        student.teach();
-        student.study();
+
+   
     }
 }
