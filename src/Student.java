5db2863774af9bc3c962285e777afba541b01f59
who(true) <alexkjh2011@gmail.com>
2022-08-29 00:37:55 +0900

    edit Student

--- src/Student.java
+++ src/Student.java
@@ -1,13 +1,11 @@
 public class Student extends Teacher{
 
-     public Student(String a){
-         System.out.println("study");
-     }
+
      public void study(){
          System.out.println("study");
      }
 
      public Student(){
-
+    super(5);
      }
 }
