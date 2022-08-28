a291fcca8bc54b743f271f90246e40be68562612
who(true) <alexkjh2011@gmail.com>
2022-08-29 00:31:52 +0900

    put int knowledge

--- src/Student.java
+++ src/Student.java
@@ -1,8 +1,12 @@
 public class Student extends Teacher{
+    int knowledge;
+
      public Student(String a){
          System.out.println("study");
      }
      public void study(){
          System.out.println("study");
      }
+
+     
 }
