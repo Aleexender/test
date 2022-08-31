4c2fe8e33d7dc65a0e21c0f8be79af4212e8bb76
who(true) <alexkjh2011@gmail.com>
2022-09-01 01:49:42 +0900

    Genetic

---
+++ src/Genetic.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Genetic {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList(); // <> 로 제한을 걸어둔다
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        int[] array = new int[3];
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+        System.out.println(array);
+        for(int i = 0; i < array.length; i++){
+            System.out.println(array[i]);
+        }
+
+
+        for(int i = 0; i < list.size(); i++){
+            Integer a = 1 + list.get(i);
+            System.out.println(list.get(i));
+        }
+        for(Integer a : list){
+            Integer b = 1 + a;
+            System.out.println(a);
+        }
+
+        System.out.println(list);
+
+        int z = 3;
+        String x = String.valueOf(z);
+    }
+}
