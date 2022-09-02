a7db72f89b0aadd3986896e4764b94bc9fc4d999
who(true) <alexkjh2011@gmail.com>
2022-09-03 01:37:15 +0900

    delete new Student num

--- src/SchoolMain.java
+++ src/SchoolMain.java
@@ -1,7 +1,7 @@
 public class SchoolMain {
     public static void main(String[] args) {
 
-   Student student = new Student(5);
+   Student student = new Student();
    Teacher teacher = new Teacher(30);
 
     }
