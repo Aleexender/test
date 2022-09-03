671d4ac1b701e10578b61469ab247d60766ebac0
who(true) <alexkjh2011@gmail.com>
2022-09-04 00:00:59 +0900

    make String array

--- src/arrayclass/Array.java
+++ src/arrayclass/Array.java
@@ -9,3 +9,12 @@ public class Array {
         array[0] = 3;
         array[2] = 2;
 //        array[3] = 1;
+
+        String[] a = new String[6];
+        a[0] = "AbCD";
+        a[1] = "EFG";
+        a[2] = "abcd";
+        a[3] = "efg";
+        a[4] = "alex";
+        a[5] = "king";
+        System.out.println(String.valueOf(a));
