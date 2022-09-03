984afb82d22d284c786fc68e2a72659f1567f4e9
who(true) <alexkjh2011@gmail.com>
2022-09-04 00:00:18 +0900

    make array

---
+++ src/arrayclass/Array.java
@@ -0,0 +1,11 @@
+package arrayclass;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] array = new int[3];
+        array[0] = 3;
+        array[2] = 2;
+//        array[3] = 1;
