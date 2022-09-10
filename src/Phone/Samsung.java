c016a845b0d7856dea64fd92dd479a271ee9f56b
who(true) <alexkjh2011@gmail.com>
2022-09-10 21:49:47 +0900

    samsung record method

--- src/Phone/Samsung.java
+++ src/Phone/Samsung.java
@@ -5,3 +5,7 @@ public class Samsung extends Smartphone {
     public void call() {
         System.out.println("SM phone ringing");
     }
+    public void record(){
+        System.out.println("recording possible");
+    }
+}
