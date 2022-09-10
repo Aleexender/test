8c47785f244b4930308d89df414bc32196f88fea
who(true) <alexkjh2011@gmail.com>
2022-09-10 21:50:16 +0900

    abstract Smartphone

---
+++ src/Phone/Smartphone.java
@@ -0,0 +1,12 @@
+package Phone;
+
+public abstract class Smartphone {
+
+    public abstract void call();
+
+    public abstract void record();
+
+    public void texting(){
+        System.out.println("i am texting");
+    }
+}
