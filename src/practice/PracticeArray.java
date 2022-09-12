0276cc3a65706fc2c9e232d2795eeab43c406159
who(true) <alexkjh2011@gmail.com>
2022-09-13 01:57:15 +0900

    array

---
+++ src/practice/PracticeArray.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class PracticeArray {
+    public static void main(String[] args) {
+        int[] array = new int[4];
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+        array[3] = 4;
+        array[4] = 5;
+    }
+}
