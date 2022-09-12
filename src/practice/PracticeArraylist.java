ef1ed05db75777e828c2ef2e6c4b4a713006697e
who(true) <alexkjh2011@gmail.com>
2022-09-13 02:18:42 +0900

    Arraylist new arraylist

---
+++ src/practice/PracticeArraylist.java
@@ -0,0 +1,10 @@
+package practice;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class PracticeArraylist {
+    public static void main(String[] args) {
+       ArrayList <Integer> arraylist = new ArrayList<Integer>();
+       arraylist.add(3);
+       arraylist.add(2);
