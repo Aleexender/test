3a5caa46718d6f18fcef4644ad4530ffbf815c76
who(true) <alexkjh2011@gmail.com>
2022-09-13 01:56:53 +0900

    Hash how to delete 중복

---
+++ src/Hash.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Hash {
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>(Arrays.asList(new String[]{"a", "b", "c", "a"}));
+        HashSet<String> set = new HashSet();
+        for(String a : list) {
+            set.add(a);
+        }
+    }
+}
