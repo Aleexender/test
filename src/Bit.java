0612a0531911f8449e040b6119a83659dcf174a4
who(true) <alexkjh2011@gmail.com>
2022-09-13 01:57:07 +0900

    practice

---
+++ src/Bit.java
@@ -0,0 +1,51 @@
+public class Bit{
+    public static void main(String[] args) {
+        int a = 2147483647;
+        int b = 2;
+
+        long c = a;
+        long d = 2147483647;
+        int e = (int) d;
+
+
+        int q = 0;
+//        boolean w =(boolean)q;
+//        int q =(int) w;
+
+        char z = 'a';
+        int r = (int) z;
+
+        for (int i = 0; i <= 10; i++) {
+            if (i % 3 == 0 || i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+        String m = "a";
+        boolean t = m instanceof Object;
+
+        int k = (a - b) > 0 ? 40 : 50;
+        String j = "민우쌤";
+        String as = "정훈";
+        if (6 % 2 == 0) {
+            System.out.println("같다");
+        }
+        if (6 % 3 == 0) {
+            System.out.println("같냐?");
+        } else {
+            System.out.println("같지 않다");
+        }
+        int fuck = 6;
+
+//        switch (true){
+//            case(fuck % 3 == 0 ):
+//                System.out.println("ㅆㅃ");
+//            case(6% 2 == 0):
+//                System.out.println("tlqkf");
+//            case(5 > 0):
+//                System.out.println("dddd");
+
+
+    }
+}
+
+
