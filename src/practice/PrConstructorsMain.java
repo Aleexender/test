2cb134915661325c6277b5acfce2fe24da701062
who(true) <alexkjh2011@gmail.com>
2022-09-14 01:54:34 +0900

    constructor this.carsize

---
+++ src/practice/PrConstructorsMain.java
@@ -0,0 +1,8 @@
+package practice;
+
+public class PrConstructorsMain {
+    public static void main(String[] args) {
+        PracticConstructor practicConstructor = new PracticConstructor(30);
+        practicConstructor.engine();
+    }
+}
--- src/practice/PracticConstructor.java
+++ src/practice/PracticConstructor.java
@@ -2,3 +2,6 @@ package practice;
 
 public class PracticConstructor {
     int carSize = 14;
+    public PracticConstructor(int carSize){
+        this.carSize =carSize;
+    }
