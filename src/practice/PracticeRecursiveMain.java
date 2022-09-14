99c4f6e65c55ae33c9222f8af438ee8804ffcf21
who(true) <alexkjh2011@gmail.com>
2022-09-14 20:37:53 +0900

    recursive main

---
+++ src/practice/PracticeRecursiveMain.java
@@ -0,0 +1,8 @@
+package practice;
+
+public class PracticeRecursiveMain {
+    public static void main(String[] args) {
+        int result = PracticeRecursive.recursiveSum(4);
+        System.out.println(result);
+    }
+}
