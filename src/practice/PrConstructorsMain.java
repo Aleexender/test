dc0b47eb3e869bda24ebda59d0d9f6633aca7191
who(true) <alexkjh2011@gmail.com>
2022-09-15 01:11:05 +0900

    Auto down Casting?

--- src/practice/PrConstructorsMain.java
+++ src/practice/PrConstructorsMain.java
@@ -4,5 +4,6 @@ public class PrConstructorsMain {
     public static void main(String[] args) {
         PracticConstructor practicConstructor = new PracticConstructor(30);
         practicConstructor.engine();
+        PracticeConstructorII practiceConstructorII = (PracticeConstructorII) practicConstructor;
     }
 }
