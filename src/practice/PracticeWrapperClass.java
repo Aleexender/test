6baec233f906ac7475fbfe4382229553aa895326
who(true) <alexkjh2011@gmail.com>
2022-09-15 20:41:21 +0900

    Integer & boxing and unboxing

--- src/practice/PracticeWrapperClass.java
+++ src/practice/PracticeWrapperClass.java
@@ -2,3 +2,9 @@ package practice;
 
 public class PracticeWrapperClass {
     public static void main(String[] args) {
+        Integer a = 5;
+        int alex = 1;
+        Integer num = new Integer(6);
+        int m = num.intValue(); // int m = num; -오토 언박싱
+    }
+}
