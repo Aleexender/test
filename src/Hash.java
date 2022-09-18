82f255249964da9e340c7ea020f2fb5bb634c3df
who(true) <alexkjh2011@gmail.com>
2022-09-19 03:35:42 +0900

    think about why outcome is true

--- src/Hash.java
+++ src/Hash.java
@@ -9,5 +9,11 @@ public class Hash {
             set.add(a);
         }
         System.out.println(set);
+
+        List list2 = new ArrayList();
+        for(int b = 0; b < list.size(); b++){
+            System.out.println (list2.add(b)); //이거 왜 다 트루로 나오는지 고민해보기
+
+        }
     }
 }
