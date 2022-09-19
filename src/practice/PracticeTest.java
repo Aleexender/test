beb5f1d12b972f9fb6432bc4648c2779f0797e4f
who(true) <alexkjh2011@gmail.com>
2022-09-20 02:33:29 +0900

    possible to use without void

--- src/practice/PracticeTest.java
+++ src/practice/PracticeTest.java
@@ -10,19 +10,27 @@ public class PracticeTest {
     List<String> list = new ArrayList<>(Arrays.asList(new String[]{"a", "b", "c", "a"}));
     List list2 = new ArrayList();
 
-    public int sad() {
-
-
-        for (int b = 0; b < list.size(); b++) {
-            System.out.println(list2.add(b));//이거 왜 다 트루로 나오는지 고민해보기
-            System.out.println(list2.toString());
-
-
+    public int sad(String a) {
+        System.out.println("i am really " + a);
+        if("".equals(a)){
+            return 1;
         }
         return 0;
     }
     public String toString(){
-        return list2.toString();
+        return list2.toString(); // toString() 괄호 안에 뭐를 넣을수있을까? 앞에 있는거에 따라 달라질거같은데
+    }
+    public void checking(){
+        Integer test = 0;
+        test.toString(12);
+//        String ts = "a";
+//        ts.toString("as"); //불가
+        Boolean as = true;
+        as.toString(false);
+//        int ts2 = 10;
+//        ts2.toString();  //불가
+
+
     }
 
 }
