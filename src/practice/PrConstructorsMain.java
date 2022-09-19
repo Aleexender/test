e1fa7cdc8fe6aeff825f221685e30742f4bac7b2
who(true) <alexkjh2011@gmail.com>
2022-09-20 02:34:07 +0900

    value of public int (main)

--- src/practice/PrConstructorsMain.java
+++ src/practice/PrConstructorsMain.java
@@ -4,6 +4,10 @@ public class PrConstructorsMain {
     public static void main(String[] args) {
         PracticConstructor practicConstructor = new PracticConstructor(30);
         practicConstructor.engine();
-        PracticeConstructorII practiceConstructorII = (PracticeConstructorII) practicConstructor;
+//        PracticeConstructorII practiceConstructorII = (PracticeConstructorII) practicConstructor;
+
+
+        PracticeTest practiceTest = new PracticeTest();
+        System.out.println(2 + practiceTest.sad(""));
     }
 }
