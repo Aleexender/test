8f13707ac3914b94e5a9b1d6c2167046b99227a4
who(true) <alexkjh2011@gmail.com>
2022-09-24 04:31:58 +0900

    list stream and enhanced for in ForMain

--- src/ForMain.java
+++ src/ForMain.java
@@ -13,3 +13,12 @@ public class ForMain {
         for(int i =0; i < list.size();i++){
             System.out.println(list.get(i));
         }
+        for(Integer a:list){
+            System.out.println(a);
+        }
+
+        list.stream().forEach(a->{
+            System.out.println(a);
+        });
+    }
+}
