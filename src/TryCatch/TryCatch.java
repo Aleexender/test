659354d5ff8f0635281f0c977b99580285566c03
who(true) <alexkjh2011@gmail.com>
2022-09-24 15:38:44 +0900

    try method( if do a.equals("a") , it will be error

--- src/TryCatch/TryCatch.java
+++ src/TryCatch/TryCatch.java
@@ -4,3 +4,9 @@ public class TryCatch {
 
     public static void main(String[] args) {
         String a = null;
+
+        try{
+            System.out.println("not error");
+            if("a".equals(a)){
+                System.out.println("a=a");
+            }
