fe3dd81da444a3315416c982a50385af020f2c89
who(true) <alexkjh2011@gmail.com>
2022-09-24 16:30:35 +0900

    Import and add list in ForEnhanced class

---
+++ src/ForEnhanced.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ForEnhanced {
+    public static void main(String[] args) {
+
+       List<Integer> list = new ArrayList<>();
+       list.add(1);
+       list.add(2);
+       list.add(3);
+
