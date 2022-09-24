d3be653ae8e106cb1d8cfa50bf3e736dbf73a27a
who(true) <alexkjh2011@gmail.com>
2022-09-24 15:33:30 +0900

    for loop in ThreadMain

--- src/tread/ThreadMain.java
+++ src/tread/ThreadMain.java
@@ -6,3 +6,9 @@ import java.util.List;
 public class ThreadMain {
     public static void main(String[] args) throws InterruptedException {
         List<Integer> list = new ArrayList<>();
+        for(int i=0; i < 100; i++){
+            CustomThread customThread = new CustomThread(i,list);
+
+            customThread.start();
+
+        }
