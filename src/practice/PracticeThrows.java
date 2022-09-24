2429483ce3b77c0ddbda2111529d149806abc235
who(true) <alexkjh2011@gmail.com>
2022-09-24 23:12:14 +0900

    asd mehtod add , put try and catch

--- src/practice/PracticeThrows.java
+++ src/practice/PracticeThrows.java
@@ -4,3 +4,17 @@ public class PracticeThrows{
 
 
 
+    public static void asd() {
+        Integer a = null;
+        try{
+            if(a.equals("a")){
+                System.out.println("not error");
+            }
+//            throw new Exception();
+
+        } catch (NullPointerException npe){
+
+            System.out.println("error");
+
+        }
+    }
