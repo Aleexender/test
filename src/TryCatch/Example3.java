5fbb19bf06ce82d152625e46df0a6165bc027329
who(true) <alexkjh2011@gmail.com>
2022-09-24 15:35:35 +0900

    use try and catch for errors and throws which can migrate to other method

---
+++ src/TryCatch/Example3.java
@@ -0,0 +1,19 @@
+package TryCatch;
+
+public class Example3 {
+
+
+
+        static void handlingException() throws Exception{
+
+//            try {
+
+                throw new Exception();
+
+//            } catch (NumberFormatException e) {
+
+//                System.out.println("호출된 메소드에서 예외가 처리됨!");
+//
+//            }
+
+        }
