1040460ee8f45c02c0424297399a548a8ae8f113
who(true) <alexkjh2011@gmail.com>
2022-09-24 15:32:54 +0900

    make a instance in ThreadMain

--- src/tread/ThreadMain.java
+++ src/tread/ThreadMain.java
@@ -3,3 +3,6 @@ package tread;
 import java.util.ArrayList;
 import java.util.List;
 
+public class ThreadMain {
+    public static void main(String[] args) throws InterruptedException {
+        List<Integer> list = new ArrayList<>();
