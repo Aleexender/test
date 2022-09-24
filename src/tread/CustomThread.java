1f750d3cb24f51a61b4ed0d9063b83196926bd20
who(true) <alexkjh2011@gmail.com>
2022-09-24 15:31:27 +0900

    make a constructor In CustomThread

--- src/tread/CustomThread.java
+++ src/tread/CustomThread.java
@@ -5,3 +5,10 @@ import java.util.List;
 public class CustomThread extends Thread{
     int a;
     List<Integer> list;
+
+
+     public CustomThread(int a, List<Integer> list){
+        this.a = a;
+        this.list = list;
+
+     }
