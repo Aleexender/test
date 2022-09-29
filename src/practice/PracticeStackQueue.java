7066df6947c3482407d12e5cf3d002917b0bb52a
who(true) <alexkjh2011@gmail.com>
2022-09-29 20:31:35 +0900

    Stack in PracticeStack

--- src/practice/PracticeStackQueue.java
+++ src/practice/PracticeStackQueue.java
@@ -8,3 +8,9 @@ public class PracticeStackQueue {
     public static void main(String[] args) {
         Stack<Integer> stack = new Stack<Integer>();
         Queue<Integer> queue = new LinkedList<Integer>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.add(5);
+
