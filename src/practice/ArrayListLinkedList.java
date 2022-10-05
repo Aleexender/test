797ea8688c5e213d72052b364aee4ab4b5cfc07d
who(true) <alexkjh2011@gmail.com>
2022-10-06 08:18:15 +0900

    ArrayList class and make ArrayList instance

---
+++ src/practice/ArrayListLinkedList.java
@@ -0,0 +1,7 @@
+package practice;
+
+import java.util.ArrayList;
+
+public class ArrayListLinkedList {
+    public static void main(String[] args) {
+        ArrayList<Integer> integerArrayList = new ArrayList<>();
