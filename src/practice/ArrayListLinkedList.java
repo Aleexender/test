e169ce8f853385643bbeafbf67726a5d12834113
who(true) <alexkjh2011@gmail.com>
2022-10-06 08:18:56 +0900

    use add, set, remove and get in Arraylist class

--- src/practice/ArrayListLinkedList.java
+++ src/practice/ArrayListLinkedList.java
@@ -5,3 +5,17 @@ import java.util.ArrayList;
 public class ArrayListLinkedList {
     public static void main(String[] args) {
         ArrayList<Integer> integerArrayList = new ArrayList<>();
+
+        integerArrayList.add(1);
+        integerArrayList.add(2);
+        integerArrayList.add(0,3);
+
+        integerArrayList.set(0,4);
+        integerArrayList.remove(0);
+        integerArrayList.get(1);
+
+        System.out.println(integerArrayList);
+//        System.out.println(integerArrayList.get(1));
+
+    }
+}
