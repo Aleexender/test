17eedcee5d173fcd1e08fcc845bec5ef1a4a77c6
who(true) <alexkjh2011@gmail.com>
2022-10-06 07:07:36 +0900

    put key and value in Hashmap

--- src/HashMap123.java
+++ src/HashMap123.java
@@ -4,3 +4,9 @@ import java.util.Map;
 public class HashMap123 {
     public static void main(String[] args) {
         Map<String,Integer> map = new HashMap<>();
+
+        map.put("weq",12);
+        map.put("weq4",121);
+        map.put("weq3",122);
+        map.put("weq2",125);
+
