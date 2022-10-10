66fc0a4f1536506e7d1b69b8398fdd5fede998d7
who(true) <alexkjh2011@gmail.com>
2022-10-10 23:57:38 +0900

    call the object and use method

--- src/lamda/HelloJava.java
+++ src/lamda/HelloJava.java
@@ -5,3 +5,14 @@ public class HelloJava {
     public interface JavaCoding{
         void nowCoding();
     }
+
+    public static void main(String[] args) {
+
+        JavaCoding jc; // 객체선언
+
+        jc = () ->{
+            System.out.println("롤린 롤린");
+        };  // 꼭 ; 붙이기
+        jc.nowCoding();
+    }
+}
