558a7d61b3b8f0f794721cc29b24c3db232ff06e
who(true) <alexkjh2011@gmail.com>
2022-10-18 23:52:10 +0900

    LinkedList add()

--- src/linkedList/Linkedlist.java
+++ src/linkedList/Linkedlist.java
@@ -5,3 +5,8 @@ import java.util.LinkedList;
 public class Linkedlist {
     public static void main(String[] args) {
         LinkedList<Integer> linkedList = new LinkedList<>();
+
+        linkedList.addFirst(1); // 가장 앞에 데이터 추가
+        linkedList.addLast(8); // 가장 뒤에 데이터 추가
+        linkedList.add(3); // 데이터 추가
+        linkedList.add(2,4); // 원하는 인덱스에 데이터 추가
