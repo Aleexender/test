ffa07b05bb332df1f0db9c3d191bc176cd4b5901
who(true) <alexkjh2011@gmail.com>
2022-10-18 23:52:23 +0900

    remove()method

--- src/linkedList/Linkedlist.java
+++ src/linkedList/Linkedlist.java
@@ -10,3 +10,9 @@ public class Linkedlist {
         linkedList.addLast(8); // 가장 뒤에 데이터 추가
         linkedList.add(3); // 데이터 추가
         linkedList.add(2,4); // 원하는 인덱스에 데이터 추가
+
+//        linkedList.removeFirst(); // 가장 앞에있는 데이터 제거
+//        linkedList.removeLast(); // 가장 뒤에있는 데이터 제거
+//        linkedList.remove(); // 생략시 0번째 인덱스 제거
+////        linkedList.remove(1); //1번째 데이터 제거
+//        linkedList.clear(); // 모든 데이터 제거
