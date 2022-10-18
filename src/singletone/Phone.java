a79e9216c70debece495fd226e5ea161690bd22d
who(true) <alexkjh2011@gmail.com>
2022-10-18 23:51:09 +0900

    make a getInstance method

--- src/singletone/Phone.java
+++ src/singletone/Phone.java
@@ -4,3 +4,11 @@ public class Phone { //Singletone
     private static Phone phone = null;
 
     private Phone(){}
+
+    public static Phone getInstance(){
+        if(phone==null){
+            phone = new Phone();
+        }
+        return phone;
+    }
+}
