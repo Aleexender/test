416832bad1c3fafca06d7714cb2232b9434497f7
who(true) <alexkjh2011@gmail.com>
2022-10-28 23:51:42 +0900

    make a hashmap instance in KeySetMethod

--- src/HashKeySet/KeySetMethod.java
+++ src/HashKeySet/KeySetMethod.java
@@ -3,3 +3,5 @@ package HashKeySet;
 import java.util.HashMap;
 
 public class KeySetMethod {
+    public static void main(String[] args) {
+        HashMap<Integer, String> hashMap = new HashMap<>();
