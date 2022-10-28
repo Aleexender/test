c95bf1e193dd6239334477cd5db6f62590adabf5
who(true) <alexkjh2011@gmail.com>
2022-10-28 23:52:05 +0900

    put values and key in hashMap

--- src/HashKeySet/KeySetMethod.java
+++ src/HashKeySet/KeySetMethod.java
@@ -5,3 +5,7 @@ import java.util.HashMap;
 public class KeySetMethod {
     public static void main(String[] args) {
         HashMap<Integer, String> hashMap = new HashMap<>();
+        hashMap.put(1, "one");
+        hashMap.put(2, "two");
+        hashMap.put(3, "three");
+        hashMap.put(4, "four");
