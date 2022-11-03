35fc52be4a9ec9647ffd2448a99db0c11bb36eb0
김민우 <rlaalsdn4564@hanmail.net>
2022-11-03 23:29:43 +0900

    test

--- src/jobs/MainLauncher.java
+++ src/jobs/MainLauncher.java
@@ -2,6 +2,7 @@ package jobs;
 
 public class MainLauncher {
     public static void main(String[] args) {
+        //test
         Person person = new Person("민우", 30, 176);
         person.introduceName();
         person = new Nogada("정훈", 24, 185);
